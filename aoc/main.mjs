d2f0df6bb74e91ae878af234b92bb55468fcc0d3
marinos <marinosch.99981@gmail.com>
2025-03-15 11:52:10 +0100

    minor change on main and config

--- aoc/main.mjs
+++ aoc/main.mjs
@@ -3,16 +3,17 @@ import {readInput} from './inputreader.js'
 
 // must be run from ./aoc - all imports are relative to this dir 
 
-const initCompleteMsg = '=> Initialisation complete'
-const argv = process.argv.slice(2) 
-const day = argv[0]
-const test = argv[1] === '-test'
+const initCompleteMsg = '=> Initialisation complete';
+const argv = process.argv.slice(2) ;
+const day = argv[0];
+const test = argv[1] === '-test';
 
-console.log(Config.message)
-const solver = Config.getSolver(day)
-solver.initialise(readInput(day, Config.year, test))
-console.log(initCompleteMsg)
+console.log(Config.message);
+const solver = Config.getSolver(day);
+solver.initialise(readInput(day, Config.year, test));
+console.log(initCompleteMsg);
 for (let i of [1, 2]) {
-    const result = solver[`part${i}`]
-    console.log(`=> Part${i} solved. Result: ${result.value} in ${result.elapsed} msec`)
+    const result = solver[`part${i}`];
+    console.log(`=> Part${i} solved. Result: ${result.value} in ${result.elapsed} msec`);
 }
+console.log('');
